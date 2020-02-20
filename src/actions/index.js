5220e2c6c8a307b3e2e2db23337f2507501ffbfb
walidoc <walidrahali10@gmail.com>
2020-02-20 12:48:12 +0100

    add secretWord action creator and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,10 @@
-import { getLetterMatchCount } from '../helpers'
+import axios from 'axios';
+
+import { getLetterMatchCount } from '../helpers';
+
 export const CORRECT_GUESS = 'CORRECT_GUESS';
 export const GUESS_WORD = 'GUESS_WORD';
+export const SET_SECRET_WORD = 'SET_SECRET_WORD';
 
 export const guessWord = (guessedWord) => (dispatch, getState) => {
   const secretWord = getState().secretWord;
@@ -9,4 +13,12 @@ export const guessWord = (guessedWord) => (dispatch, getState) => {
   if(guessedWord === secretWord) {
     dispatch({type: CORRECT_GUESS});
   }
+}
+
+export const getSecretWord = () => dispatch => {
+  return axios.get('http://localhost:3030')
+    .then(response => dispatch({
+      type: SET_SECRET_WORD,
+      payload: response.data
+    }))
 }
--- src/reducers/secretWordReducer.js
+++ src/reducers/secretWordReducer.js
@@ -1,3 +1,13 @@
+import { SET_SECRET_WORD } from '../actions'
+
 export default (state=null, action) => {
+  switch (action.type) {
+    case SET_SECRET_WORD:
+      state = action.payload;
+      break;
+  
+    default:
+      break;
+  }
   return state;
 }
